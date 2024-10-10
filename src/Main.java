55085fee8a359e58574ab0c8681da8815d94ff6e
Jan kowalski <jan.kowalski@pollub.edu.pl>
2024-10-10 17:00:20 +0200

    3 commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World123");
+        System.out.println("Hello World111");
+        System.out.println("Hello World222");
+    }
+}
