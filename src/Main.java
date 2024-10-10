269ee889e576ec4a798080b80c691233c8bb6e34
Jan kowalski <jan.kowalski@pollub.edu.pl>
2024-10-10 16:57:05 +0200

    git init

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome1");
         System.out.println("Hello and welcome2");
+        System.out.println("Hello and welcome3");
+
 
     }
 }
